898a67b9c672d78e26961d4eb8c2ab23704832f1
NCAZO <nicolas.cazo@gustaveroussy.fr>
2024-06-17 08:19:10 +0200

    FIX BACK ENV VAR FIX Lost connexion TOKEN after F5 FIX Responsive Cards Comment FIX SideBar FIX Cards articles size FIX Cards thèmes size FIX Cards abonnement size FIX Save user password on modif FIX Sub to theme FIX thème button FIX Add thème to select CreateArticle FIX Trier par FIX Comments article FIX Change button s'abonner when abonné

--- front/src/app/_guards/auth.guard.ts
+++ front/src/app/_guards/auth.guard.ts
@@ -1,23 +1,20 @@
 import {Injectable} from "@angular/core";
 import {CanActivate, Router} from "@angular/router";
-import {SessionService} from "../_services/session/session.service";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-    constructor(
-        private router: Router,
-        private sessionService: SessionService,
-    ) { }
+  constructor(
+    private router: Router,
+  ) {
+  }
 
   public canActivate(): boolean {
-    if (this.sessionService.isLogged) {
-      //this.router.navigate(['session']);
+    if (localStorage.getItem("token")) {
       return true;
-    }
-    else{
+    } else {
       this.router.navigate(['login']);
       return false;
     }
